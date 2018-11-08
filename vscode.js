08fb6a5c35e22abd02d22ce95a1bb49c063c83b1
Nathan Thorpe <nath.thorpe@gmail.com>
2018-11-08 17:09:37 +1100

    Add files

---
+++ vscode.js
@@ -0,0 +1,33 @@
+{
+  "typescript.referencesCodeLens.enabled": false,
+  "gitlens.advanced.messages": {
+    "suppressShowKeyBindingsNotice": true
+  },
+  "gitlens.historyExplorer.enabled": true,
+  "workbench.colorTheme": "Tomorrow Night Blue",
+  "editor.glyphMargin": false,
+  "eslint.enable": true,
+  "eslint.validate": [
+    {
+      "autoFix": true,
+      "language": "javascript"
+    },
+    {
+      "autoFix": true,
+      "language": "vue"
+    }
+  ],
+  "window.zoomLevel": 0,
+  "editor.tabSize": 2,    
+  "emmet.syntaxProfiles": {
+    "vue-html": "html",
+    "vue": "html"
+  },
+  "emmet.showAbbreviationSuggestions": true,
+  "emmet.showExpandedAbbreviation": "always",
+  "emmet.includeLanguages": {
+    "vue-html": "html",
+    "vue": "html",
+    "js": "html"
+  },
+}
