61c00abe3a2cd832c6d6473cd26e014ac8a265a0
Nathan Thorpe <nath.thorpe@gmail.com>
2018-11-29 14:25:09 +1100

    Create vscode-keybindings.js

---
+++ vscode-keybindings.js
@@ -0,0 +1,10 @@
+[
+  {
+    "key": "ctrl+shift+right",
+    "command": "editor.emmet.action.balanceOut"
+  },
+  {
+    "key": "ctrl+shift+left",
+    "command": "editor.emmet.action.balanceIn"
+  }
+]
