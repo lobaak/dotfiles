d3fefb37b04e0a6a0c8a269ae143543b5a7ad1ac
Nathan Thorpe <nathan@today.design>
2019-06-18 10:53:38 +1000

    Update vscode.js

--- vscode.js
+++ vscode.js
@@ -1,8 +1,56 @@
 {
   "typescript.referencesCodeLens.enabled": false,
-  "workbench.colorTheme": "Tomorrow Night Blue",
+  "window.zoomLevel": 0,
+  "editor.insertSpaces": true,
+  "editor.detectIndentation": true,
+  "editor.suggestSelection": "first",
   "editor.glyphMargin": false,
+  "editor.tabSize": 2,
+  "workbench.colorTheme": "Tomorrow Night Blue",
+  "workbench.activityBar.visible": true,
+  "workbench.sideBar.location": "left",
+  "files.trimTrailingWhitespace": true,
+  "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue",
+  "importCost.javascriptExtensions": ["\\.jsx?$"],
+  // Git
+  "git.autofetch": true,
+  "gitlens.views.fileHistory.enabled": true,
+  "gitlens.views.lineHistory.enabled": true,
+  "gitlens.advanced.messages": {
+    "suppressShowKeyBindingsNotice": true
+  },
+  // Emmet
+  "emmet.syntaxProfiles": {
+    "vue-html": "html",
+    "vue": "html"
+  },
+  "emmet.showAbbreviationSuggestions": true,
+  "emmet.showExpandedAbbreviation": "always",
+  "emmet.triggerExpansionOnTab": true,
+  "emmet.includeLanguages": {
+    "vue-html": "html",
+    "vue": "html",
+    "js": "html",
+    "ss": "html",
+    "Silverstripe": "html"
+  },
+  // Search
+  "search.location": "sidebar",
+  "search.exclude": {
+    "**/node_modules": true
+  },
+  "search.useIgnoreFiles": true,
+  // Styles
+  "stylelint.enable": true,
+  "[scss]": {
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.formatOnSave": true
+  },
+  "scss.validate": false,
+  "css.validate": false,
+  // ESLint
   "eslint.enable": true,
+  "eslint.autoFixOnSave": true,
   "eslint.packageManager": "yarn",
   "eslint.validate": [
     {
@@ -18,35 +66,4 @@
       "language": "javascriptreact"
     }
   ],
-  "window.zoomLevel": 0,
-  "editor.tabSize": 2,
-  "editor.insertSpaces": true,
-  "editor.detectIndentation": true,
-  "emmet.syntaxProfiles": {
-    "vue-html": "html",
-    "vue": "html"
-  },
-  "emmet.showAbbreviationSuggestions": true,
-  "emmet.showExpandedAbbreviation": "always",
-  "emmet.triggerExpansionOnTab": true,
-  "emmet.includeLanguages": {
-    "vue-html": "html",
-    "vue": "html",
-    "js": "html",
-    "ss": "html",
-    "Silverstripe": "html",
-  },
-  "gitlens.views.fileHistory.enabled": true,
-  "gitlens.views.lineHistory.enabled": true,
-  "gitlens.advanced.messages": {
-    "suppressShowKeyBindingsNotice": true
-  },
-  "importCost.javascriptExtensions": [
-    "\\.jsx?$"
-  ],
-  "git.autofetch": true,
-  "search.location": "sidebar",
-  "workbench.activityBar.visible": true,
-  "workbench.sideBar.location": "left",
-  "files.trimTrailingWhitespace": true,
 }
